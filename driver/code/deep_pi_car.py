23e23b79a24c388cdda4c0fd7cf99ccf8af408a1
khu834 <khu834@gmail.com>
2020-01-12 16:21:00 -0800

    remove redundant files

--- driver/code/deep_pi_car.py
+++ driver/code/deep_pi_car.py
@@ -48,7 +48,6 @@ class DeepPiCar(object):
 
         self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
         datestr = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
-        self.video_orig = self.create_video_recorder('../data/car_video%s.avi' % datestr)
         self.video_lane = self.create_video_recorder('../data/car_video_lane%s.avi' % datestr)
         self.video_objs = self.create_video_recorder('../data//car_video_objs%s.avi' % datestr)
 
@@ -75,7 +74,6 @@ class DeepPiCar(object):
         self.back_wheels.speed = 0
         self.front_wheels.turn(90)
         self.camera.release()
-        self.video_orig.release()
         self.video_lane.release()
         self.video_objs.release()
         cv2.destroyAllWindows()
